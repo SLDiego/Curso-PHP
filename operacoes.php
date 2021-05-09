4695fc38bc7e4c47c0c9416e514df0892eb25f60
Diego SL <sldiegoti@gmail.com>
2021-05-08 22:00:37 -0300

    Operações, tipos de Dados, variaveis

---
+++ operacoes.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$idade = 30;
+$idadeDaqui10Anos = $idade + 10;
+
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 * 2;
+
+$doisAoCubo = 2 ** 3; //Exponencial
+
+$RestoDaDivisão = 10 % 3; //resto da Divisão
+
+
+
+
+
+echo $RestoDaDivisão;
---
+++ tiposdeDados.php
@@ -0,0 +1,16 @@
+<?php
+
+//PHP descobre os tipos de variáveis dinamicamente (tipagem dinâmica)
+
+
+$idade = 30; // Inteiro
+
+$salario = 1000.30; //float, double
+
+$Texto = "ola mundo"; //string
+
+
+$verdadeiro = true; //boolean
+$falso = false;
+
+echo gettype($salario); //Utilizando gettype conseguimos descobrir o tipo de uma variável
