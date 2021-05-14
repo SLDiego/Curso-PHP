1ef1c0e88534bc8efb4d51396e790215945f6233
Diego SL <sldiegoti@gmail.com>
2021-05-13 23:34:36 -0300

    Calculo Imc

---
+++ desafio3IMC.php
@@ -0,0 +1,20 @@
+<?php
+
+$peso = 90;
+$altura = 1.74;
+$imc = $peso / $altura ** 2;
+
+echo "Seu IMC é de $imc." .PHP_EOL; 
+echo "Você está com o IMC ";
+
+if ($imc < 18.5) {
+    echo "abaixo";
+} elseif ($imc < 25) {
+    echo "dentro";
+} else {
+    echo "acima";
+}
+
+echo " do recomendado";
+
+	
