3e6e98b4fd6ca3bbb5f528d431a3317315c5ee83
Diego SL <sldiegoti@gmail.com>
2021-05-18 20:34:12 -0300

    banco-deposito-saque

--- banco.php
+++ banco.php
@@ -1,33 +1,62 @@
 <?php
 
-//foreach -> outra forma de realizar loops (repetições), específica para trabalhar com listas de dados (normalmente, arrays).
-//com a funcão foreach -> Não precisamos conhecer as chaves de cada item para percorrer seus valores  
-// podemos percorrer arrays associativos sem nenhum problema. Podemos inclusive recuperar o valor de seus índices dentro do loop.
+// Uma função retorna um valor, ou seja, a chamada da função representa um valor após sua execução. 
+// Uma subrotina apenas executa um código isolado.
 
 
-$contasCorrentes = [
-	'123.456.789-10' =>  [
-	'titular' => 'Vinicius',
-	'saldo' => 100
+function exibeMensagem(string $mensagem){
+    echo $mensagem . PHP_EOL;
+}
 
-	], 
-	'123.456.789-11' =>[
-	'titular' => 'Maria',
-	'saldo' => 10000	
-	], 
-	'123.456.789-12' =>[
-	'titular' => 'Alberto',
-	'saldo' => 300
-	]	
-];
+function sacar(array $conta, float $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
 
-$contasCorrentes['123.456.789-11']['saldo'] -= 500;
-
-
-foreach ($contasCorrentes as $cpf => $conta) {
-	echo $cpf . " " . $conta['titular'] . '	' . $conta['saldo'] .PHP_EOL;
-	
+    return $conta;
 }
 
 
-	
+
+function depositar(array $conta, float $valorADepositar)
+{
+	if ($valorADepositar < 0) {
+		echo "Você não pode fazer um deposito negativo" .PHP_EOL;
+	} else{
+		$conta['saldo'] += $valorADepositar;
+	}
+	return $conta;
+}
+
+
+
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.789-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+
+$contasCorrentes['123.256.789-12'] = depositar($contasCorrentes['123.256.789-12'], 95);
+
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
+$contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
+
+
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+}
